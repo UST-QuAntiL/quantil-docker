037b4bfa246ae3c897623d2c59a5e06f7efaf310
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-10-26 14:39:16 +0100

    Enable cloning repository from certain branch

--- db/clone-data-repo.sh
+++ db/clone-data-repo.sh
@@ -10,14 +10,14 @@ if test -f "$FILE"; then
     chmod 400 /root/.ssh/id_rsa && ssh-keyscan github.com >> /root/.ssh/known_hosts
 
     # clone repo, if successful copy setup script to /docker-entrypoint-initdb.d/
-    git clone ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
+    git clone --single-branch --branch ${QC_ATLAS_CONTENT_REPOSITORY_BRANCH} ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
 	if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${QC_ATLAS_SUBFOLDER_CONTENT_BACKUP_FILES}" ]; then
         cp setup-atlas.sh /docker-entrypoint-initdb.d/
         echo "qc-atlas-content repo was cloned successfully"
 
         # even though this repo is actually public the data heavily depends on the data of qc-atlas
         # therefore it is only loaded if the atlas repo was sucessfully cloned
-        git clone ${NISQ_CONTENT_REPOSITORY_URL} ${NISQ_CONTENT_REPOSITORY_PATH}
+        git clone --single-branch --branch ${NISQ_CONTENT_REPOSITORY_BRANCH} ${NISQ_CONTENT_REPOSITORY_URL} ${NISQ_CONTENT_REPOSITORY_PATH}
         if [ -d "${NISQ_CONTENT_REPOSITORY_PATH}/${NISQ_SUBFOLDER_CONTENT_BACKUP_FILES}" ]; then
             cp setup-nisq.sh /docker-entrypoint-initdb.d/
             echo "nisq-analyzer-content repo was cloned successfully"
@@ -33,7 +33,7 @@ else
 fi
 
  # clone repo, if successful copy setup script to /docker-entrypoint-initdb.d/
-git clone ${PATTERNATLAS_CONTENT_REPOSITORY_URL} ${PATTERNATLAS_CONTENT_REPOSITORY_PATH}
+git clone --single-branch --branch ${PATTERNATLAS_CONTENT_REPOSITORY_BRANCH} ${PATTERNATLAS_CONTENT_REPOSITORY_URL} ${PATTERNATLAS_CONTENT_REPOSITORY_PATH}
 if [ -d "${PATTERNATLAS_CONTENT_REPOSITORY_PATH}/${PATTERNATLAS_SUBFOLDER_CONTENT_BACKUP_FILES}" ]; then
     cp setup-patternatlas.sh /docker-entrypoint-initdb.d/
     echo "pattern-atlas-content repo was cloned successfully"
