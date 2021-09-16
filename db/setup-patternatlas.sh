04029973bd3d8ee02794772444d9676c5cae7cf2
Lukas Harzenetter <lharzenetter@gmx.de>
2021-09-16 12:53:37 +0200

    first ides

--- db/setup-patternatlas.sh
+++ db/setup-patternatlas.sh
@@ -2,12 +2,16 @@
 cd ${PATTERNATLAS_CONTENT_REPOSITORY_PATH}
 cd ${PATTERNATLAS_SUBFOLDER_CONTENT_BACKUP_FILES}
 
+echo "importing PatternAtlas schema"
+
 # import schema
 # finds first file alphabetically
 PATTERN_ALTAS_SCHEMA=$(ls -1 | head -n 1)
-psql ${PATTERNATLAS_DB} < ${PATTERN_ALTAS_SCHEMA}
+psql -U patternatlas -d ${PATTERNATLAS_DB} -a -f ${PATTERN_ALTAS_SCHEMA}
+
+echo "importing PatternAtlas data"
 
 # import data
 # finds last file alphabetically
 PATTERN_ALTAS_DATA=$(ls -1 | tail -n 1)
-psql ${PATTERNATLAS_DB} < ${PATTERN_ALTAS_DATA}
+psql  -U patternatlas -d ${PATTERNATLAS_DB} -a -f ${PATTERN_ALTAS_DATA}
